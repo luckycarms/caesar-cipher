4e304e8320ec91a9c1c5781a67b4eb7e57e1c0ea
luckycarms <caleha23@gmail.com>
2024-11-04 19:17:36 -0500

    cipher.py assignment - added code

--- cipher.py
+++ cipher.py
@@ -1,1 +1,14 @@
-# add your code here
+abc = "abcdefghijklmnopqrstuvwxyz"
+plain_text = input("Please enter a plain text sentence: ")
+plain_text = plain_text.lower()
+shift = 5
+
+encrypted_text = " "
+
+for char in plain_text:
+    if char in abc:
+        position = abc.find(char)
+        position = (position + shift) % 26
+        char = abc[position]
+    encrypted_text += char
+print(encrypted_text)
